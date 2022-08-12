2b31a6c31be7ccbc2baf652663b658c61dab067f
Scott Upman <scottupman1@gmail.com>
2022-08-12 14:43:33 -0400

    Addeed @Repository to repositories

--- src/main/java/com/conygre/backendTampa4/dao/AssetRepository.java
+++ src/main/java/com/conygre/backendTampa4/dao/AssetRepository.java
@@ -2,7 +2,9 @@ package com.conygre.backendTampa4.dao;
 
 import com.conygre.backendTampa4.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssetRepository extends JpaRepository<Asset, String> {
 
 }
--- src/main/java/com/conygre/backendTampa4/dao/TradeRepository.java
+++ src/main/java/com/conygre/backendTampa4/dao/TradeRepository.java
@@ -1,6 +1,8 @@
 package com.conygre.backendTampa4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TradeRepository extends JpaRepository {
 }
