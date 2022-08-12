232ad1515f1bbf8e4b7932e1ef7c1299adc65f9b
Scott Upman <scottupman1@gmail.com>
2022-08-12 13:55:24 -0400

    Added queries for database setup

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE `portfolio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE stock(
+    symbol VARCHAR(45) PRIMARY KEY,
+    name VARCHAR(45),
+    quantity DECIMAL
+)
+
+CREATE TABLE `stock` (
+  `symbol` varchar(45) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`symbol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `trades` (
+  `trade_id` int NOT NULL,
+  `shares` int DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `timestamp_of_trade` int DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`trade_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
