c21e66060c302d76f7318ac556379ec21c257204
akelly71 <akelly71@gatech.edu>
2022-08-12 15:45:03 -0400

    adding control and service layer changes

--- src/main/java/com/conygre/backendTampa4/service/Tampa4Service.java
+++ src/main/java/com/conygre/backendTampa4/service/Tampa4Service.java
@@ -2,13 +2,28 @@ package com.conygre.backendTampa4.service;
 
 
 import com.conygre.backendTampa4.dao.AssetRepository;
-import com.conygre.backendTampa4.dao.TradeRepository;
+import com.conygre.backendTampa4.entity.Asset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED)
 public class Tampa4Service {
+
     @Autowired
-    private AssetRepository assetRepository;
+    private AssetRepository dao;
+
+    public Collection<Asset> getAllAssets() {
+        return dao.findAll();
+    }
+
+    public Asset getAssetInfo(String symbol) {
+        return dao.getReferenceById(symbol);
+    }
+
 }
 
