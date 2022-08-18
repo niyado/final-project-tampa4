f636df2f8d744f137bb837eb8111403ca40d1f16
akelly71 <akelly71@gatech.edu>
2022-08-18 10:43:20 -0400

    updating asset controller and trade service/controller

--- src/main/java/com/conygre/backendTampa4/controller/TradeController.java
+++ src/main/java/com/conygre/backendTampa4/controller/TradeController.java
@@ -1,12 +1,32 @@
 package com.conygre.backendTampa4.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.conygre.backendTampa4.entity.Asset;
+import com.conygre.backendTampa4.service.TradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/portfolio/trade")
 public class TradeController {
 
+    @Autowired
+    private TradeService tradeService;
+
+    @GetMapping("/get/{symbol}")
+    public ResponseEntity<Asset> getTradeInfo(@PathVariable("symbol") String symbol) {
+        return null;
+    }
+
+    @GetMapping("/getAll")
+    ResponseEntity<Iterable<Asset>> getAllTrades() {
+        return null;
+    }
+
+    @PostMapping(value = "/newAsset", consumes = "application/json")
+    public void newTrade(@RequestBody Asset asset){
+
+    }
 }
--- src/main/java/com/conygre/backendTampa4/service/TradeService.java
+++ src/main/java/com/conygre/backendTampa4/service/TradeService.java
@@ -1,4 +1,8 @@
 package com.conygre.backendTampa4.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TradeService {
+
 }
